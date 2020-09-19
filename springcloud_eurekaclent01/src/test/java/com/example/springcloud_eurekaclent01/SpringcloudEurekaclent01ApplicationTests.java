43a4a0c33a839c4a5839b6ae9a1d664c85ea1d56
wangfei183 <18339813925@163.com>
2020-09-19 18:14:45 +0800

    提交eureka-server项目

---
+++ springcloud_eurekaclent01/src/test/java/com/example/springcloud_eurekaclent01/SpringcloudEurekaclent01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud_eurekaclent01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurekaclent01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springcloud_eurekaclient02/src/main/java/com/example/springcloud_eurekaclient02/SpringcloudEurekaclient02Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloud_eurekaclient02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaclient02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaclient02Application.class, args);
+    }
+
+}
